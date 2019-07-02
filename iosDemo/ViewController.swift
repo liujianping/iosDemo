5ff46456329a1242c817063b0055de6fd54e9e65
liujianping <liujianping.itech@qq.com>
2019-07-02 22:30:35 +0800

    Initial Commit

---
+++ iosDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosDemo
+//
+//  Created by Jay Liu on 2/7/2019.
+//  Copyright © 2019 GitDiG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iosDemoTests/iosDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  iosDemoTests.swift
+//  iosDemoTests
+//
+//  Created by Jay Liu on 2/7/2019.
+//  Copyright © 2019 GitDiG. All rights reserved.
+//
+
+import XCTest
+@testable import iosDemo
+
+class iosDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
