8c37a039ab08ab7cdacec55ff395dd5a4a0b9a9e
liujianping <liujianping.itech@qq.com>
2019-07-03 11:18:57 +0800

    first commit

--- iosDemo/ViewController.swift
+++ iosDemo/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let client = Hello_HelloServiceServiceClient(address: ":8833", secure: false)
+        var req = Hello_HelloRequest()
+        req.greeting = "JayL"
+        do {
+            let resp = try client.sayHello(req)
+            print("resp: \(resp.reply)")
+        } catch {
+            print("error: \(error.localizedDescription)")
+        }
     }
 
 
